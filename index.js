3cb122798c5d0294f5efb8a72e0356d93fe1eea7
Ian Flurkey <ianflurkey@gmail.com>
2023-02-06 17:41:14 -0500

    got rid of old comments, moved blog post code to bottom.

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 /*Variables*/
-let count = 0;
-//references to the search form and results divs
 const searchForm = document.getElementById('search-form');
 const resultsDiv = document.getElementById('results');
 
@@ -18,21 +16,13 @@ const search = (zipCode) => {
     })
     .then((breweries) => {
 
-      // clears the previous search results
       resultsDiv.innerHTML = '';
-
 
       if (breweries.length === 0) {
         resultsDiv.innerHTML = '<h2>No Breweries listed in this area, try another zip code</h2>';
       
       } else {
       
-        // displays the search results as a card, per card
-
-
-        /*Add a like button to each brewery card. The button should have a 0 on it.
-         When the button is clicked, the zero should increment.*/
-
       breweries.forEach((brewery) => {
         
         const card = document.createElement("div");
@@ -50,30 +40,11 @@ const search = (zipCode) => {
         <br>
         <p>Phone: ${brewery.phone || 'No phone number available'}</p>
         <p>${brewery.website_url ? `<a href="${brewery.website_url}" target="_blank">${brewery.website_url}</a>`:
-         'No Website Available'}</p>`
+        'No Website Available'}</p>`
+
         card.appendChild(header)
         card.appendChild(body)
         resultsDiv.appendChild(card)
-
-        //older code referenced in blog post
-
-        /* resultsDiv.innerHTML += `
-           <div class="card">
-             <div class="card-header">
-               <h2>${brewery.name}</h2>
-             </div>
-             <div class="card-body">
-               <p>${brewery.street || 'Not Listed'}</p>
-               <p>${brewery.city}, ${brewery.state} ${brewery.postal_code}</p>
-               <br>
-               <p>Phone: ${brewery.phone || 'No phone number available'}</p>
-               <p>${brewery.website_url ? `<a href="${brewery.website_url}" target="_blank">${brewery.website_url}</a>`:
-                'No Website Available'}</p>
-
-             </div>
-           </div>
-         `; */
-
 
       });
 
@@ -90,7 +61,6 @@ const search = (zipCode) => {
 searchForm.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  //gets the zip code that the user entered, or uses the default if no zip code was entered
   const zipCode = document.getElementById('zip-code').value || defaultZipCode;
 
 
@@ -102,8 +72,6 @@ searchForm.addEventListener('submit', (event) => {
 //reset button event listener
 document.getElementById('reset-button').addEventListener('click', () => {
 
-  //clears the page, and adds a message to get started
-
   resultsDiv.innerHTML = `
     <h2>Type a zip code above to get started</h2>
      `;
@@ -112,7 +80,7 @@ document.getElementById('reset-button').addEventListener('click', () => {
 
 
 
-  //adds functionality to search by pressing enter
+//adds functionality to search by pressing enter
 searchForm.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
     event.preventDefault();
@@ -124,10 +92,6 @@ searchForm.addEventListener('keydown', (event) => {
     search(zipCode);
   }
 });
-
-//code from line 70
-
-
 
   //adds a window resize event listener
 window.addEventListener('resize', () => {
@@ -155,3 +119,24 @@ window.addEventListener('resize', () => {
 /*Default event*/
 
 search(defaultZipCode);
+
+
+ //older code referenced in blog post, originally on line 32
+
+
+        /* resultsDiv.innerHTML += `
+           <div class="card">
+             <div class="card-header">
+               <h2>${brewery.name}</h2>
+             </div>
+             <div class="card-body">
+               <p>${brewery.street || 'Not Listed'}</p>
+               <p>${brewery.city}, ${brewery.state} ${brewery.postal_code}</p>
+               <br>
+               <p>Phone: ${brewery.phone || 'No phone number available'}</p>
+               <p>${brewery.website_url ? `<a href="${brewery.website_url}" target="_blank">${brewery.website_url}</a>`:
+                'No Website Available'}</p>
+
+             </div>
+           </div>
+         `; */
